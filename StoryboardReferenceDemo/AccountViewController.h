01852b92034fd76658926956db1d6a6791326df3
liang <603590822@qq.com>
2015-10-23 11:42:31 +0800

    first commit

---
+++ StoryboardReferenceDemo/AccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountViewController.h
+//  StoryboardReferenceDemo
+//
+//  Created by liangyiqi on 15/10/22.
+//  Copyright © 2015年 LYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountViewController : UIViewController
+
+@end
---
+++ StoryboardReferenceDemo/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainViewController.m
+//  StoryboardReferenceDemo
+//
+//  Created by liangyiqi on 15/10/22.
+//  Copyright © 2015年 LYQ. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
